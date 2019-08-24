d928c580afea89ec1fae8f6508d3c5943aaf76c6
zero-24 <zero-24@users.noreply.github.com>
2019-08-24 11:23:35 +0200

    add new options and translations

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ..
 tar -cf files.tar lib/
-tar -cf build/antispam-extended.tar xml/ files.tar package.xml
+tar -cf build/antispam-extended.tar xml/ files.tar package.xml languages/
 rm files.tar
 echo 'package ready'
